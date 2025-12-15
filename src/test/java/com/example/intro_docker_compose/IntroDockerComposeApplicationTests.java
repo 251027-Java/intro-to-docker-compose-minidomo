a54ec029b5999ac72188cb8ddd8af7d88ae24f67
JB Ladera <minidomou@gmail.com>
2025-12-15 14:33:43 -0600

    add spring boot project

---
+++ src/test/java/com/example/intro_docker_compose/IntroDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intro_docker_compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroDockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
